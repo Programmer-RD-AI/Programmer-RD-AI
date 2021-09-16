d6db3c3c932dfecce96c3358f0b96d9fe42f3515
Ranuga-D <79456372+Programmer-RD-AI@users.noreply.github.com>
2021-09-16 14:40:50 +0530

    Update README.py

--- README.py
+++ README.py
@@ -1,42 +1,61 @@
 print("""
+\n
 I am Ranuga Disansa and I love Programming and AI in general.
+\n
 I am a Self-Learning Programmer.
+\n
 I am Currently Learning Deep Learning with Python and PyTorch.
+\n
 I mostly like Self-Driving Cars kind of stuff in AI mostly but I like other stuff also.
+\n
 """)
 
-print("""If you give yourself 30 days to clean your home, it will take you 30 days. But if you give yourself 3 hours, it will take 3 hours. The same applies to your goals, ambitions, and potential.
+print("""
+\n
+If you give yourself 30 days to clean your home, it will take you 30 days. But if you give yourself 3 hours, it will take 3 hours. The same applies to your goals, ambitions, and potential.
+\n
 - Elon Musk
+\n
 """)
 
 print("""
+\n
 - Contact Me 💬
-  
+\n
   - Email - go2ranuga@gmail.com
-  
+\n
   - Github - https://github.com/Programmer-RD-AI
-  
+\n
   - LinkedIn - https://www.linkedin.com/in/ranuga-disansa-gamage-94a7671b2/
-
+\n
   - Kaggle - https://www.kaggle.com/ranugadisansagamage
-
+\n
   - PyTorch Community - https://discuss.pytorch.org/u/programmer-rd-ai/summary
-
+\n
   - Discord - Programmer-RD-AI#2022
-
+\n
   - GitMemory - https://githubmemory.com/@Programmer-RD-AI
-  
+\n  
   - Medium - https://medium.com/@Programmer-RD-AI
-  
+\n
 """)
 
 print("""
+\n
 - My Skills 💼 
+\n
   - Web Dev (ReactJS, Flask).
+  \n
   - API Dev (Flask Restful).
+  \n
   - Deep Learning (TensorFlow,PyTorch,Detectron2).
+  \n
   - Machine Learning (Sklearn).
+  \n
   - Analytics (Matplotlib, Seaborn).
+  \n
   - IOT (Arduino(C),Rasberry Pi(Python)).
+  \n
   - DataBases (Firebase,MongoDB,SQLLite,SQL).
+\n
 """)
